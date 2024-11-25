c3cd8b74adec0fb5985bc5f1a140bbf42c76e9ec
BenSLowery <benjaminslowery@outlook.com>
2024-11-25 15:15:29 +0000

    Version 0.1.2

--- src/lib.rs
+++ src/lib.rs
@@ -62,6 +62,66 @@ fn expectation(x: usize, param_one: f64, param_two: f64, st_out: f64, second_par
     Ok((exp, exp_first_stage))
 }
 
+////////////////////////////////////////
+// Old version of the lookahead policy
+////////////////////////////////////////
+/// 
+// // Implements a lookahead policy which also optimises an order quantity
+// #[pyfunction]
+// #[pyo3(signature = (x, param_one, param_two,  max_q, second_param_one=None, second_param_two=None, distribution=None, p=None,cu=None,co=None,cdfw=None))]
+// fn lookahead(x: usize, param_one: f64, param_two: f64, max_q: usize,second_param_one: Option<f64>, second_param_two: Option<f64>,distribution: Option<char>, p: Option<f64>, cu: Option<f64>, co: Option<f64>, cdfw: Option<f64>) -> PyResult<(f64,f64,usize)> {
+//     // Costs
+//     let cost_params = (cu.unwrap_or(18.0), co.unwrap_or(1.0), cdfw.unwrap_or(0.0));
+//     // Generate required parameters and pmf's
+//     let dfw_p: f64 = p.unwrap_or(0.8);
+
+
+//     // Pregenerate the binomial distribution
+//     let mut binom_pmf = [[0.0; D_MAX+1]; D_MAX+1];
+//     for i in 0..D_MAX+1 {
+//         let binom_distr = Binomial::new(dfw_p, i as u64).unwrap();
+//         for j in 0..D_MAX+1 {
+//             binom_pmf[i][j] = (binom_distr.pmf(j as u64)) as f64;
+//         }
+//     }
+
+
+
+//     let dist: char = distribution.unwrap_or('P');
+//     let d1_pmf: [f64; D_MAX] = distributions::generate_distribution::distribution(dist, param_one, second_param_one);
+//     let d2_pmf: [f64; D_MAX] = distributions::generate_distribution::distribution(dist, param_two, second_param_two);
+
+//     // Calculate the expectation
+//     let mut exp: f64 = 0.0;
+//     let mut exp_first_stage: f64 = 0.0;
+    
+
+//     // Find the optimal q
+//     let q: f64 = optimise::min_func::minimise_q(max_q, x, d1_pmf, d2_pmf, binom_pmf, cost_params);
+   
+//     // First stage shortage
+//     for (d1_val,d1_pmf_i) in d1_pmf.iter().enumerate() {
+        
+//         // First stage shortage
+//         let shortage_p1: usize = max(d1_val as isize -x as isize,0) as usize;
+//         for j in 0..shortage_p1+1  {
+//             let fs = d1_pmf_i * binom_pmf[shortage_p1][j] * (f64::max(d1_val as f64 - x as f64,0.0)- j as f64);
+//             exp_first_stage += fs;
+//             exp += fs;
+//         }
+
+//         // Second stage shortage
+//         for (d2_val, d2_pmf_i) in d2_pmf.iter().enumerate() {
+//             let shortage_p2: usize = max(d2_val as isize - max(x as isize-d1_val as isize,0)-q as isize,0) as usize;
+//             for j in 0..shortage_p2+1 {
+//                 exp += d1_pmf_i * d2_pmf_i * binom_pmf[shortage_p2][j] * (f64::max(d2_val as f64-f64::max(x as f64 -d1_val as f64,0.0)-q,0.0)-j as f64) ;
+//             }
+//         }
+//     }
+
+//     Ok((exp_first_stage, exp, q as usize))
+// }
+
 // Implements a lookahead policy which also optimises an order quantity
 #[pyfunction]
 #[pyo3(signature = (x, param_one, param_two,  max_q, second_param_one=None, second_param_two=None, distribution=None, p=None,cu=None,co=None,cdfw=None))]
@@ -93,7 +153,7 @@ fn lookahead(x: usize, param_one: f64, param_two: f64, max_q: usize,second_param
     
 
     // Find the optimal q
-    let q: f64 = optimise::min_func::minimise_q(max_q, x, d1_pmf, d2_pmf, binom_pmf, cost_params);
+    let q: f64 = optimise::min_func::minimise_q_search(max_q as f64, x, d1_pmf, d2_pmf, binom_pmf, cost_params);
    
     // First stage shortage
     for (d1_val,d1_pmf_i) in d1_pmf.iter().enumerate() {
--- src/optimise/min_func.rs
+++ src/optimise/min_func.rs
@@ -1,45 +1,118 @@
 // Find the smallest q in a one dimensional search. Does a full search of all possible q values and returns the smallest q value and the corresponding expectation. 
 
-pub fn minimise_q(max_q: usize, x: usize, demand_p1: [f64; crate::D_MAX], demand_p2: [f64; crate::D_MAX], dfw_p2: [[f64; crate::D_MAX+1]; crate::D_MAX+1], cost_params: (f64, f64, f64)) -> f64 {
-    // Create a list of acceptable order quantites
-    let mut acceptable_q: Vec<usize> = Vec::with_capacity(max_q + 1);
-    acceptable_q.extend(0..=max_q);
+
+// // Exhaustive search for the optimal q (deprecated)
+// pub fn minimise_q(max_q: usize, x: usize, demand_p1: [f64; crate::D_MAX], demand_p2: [f64; crate::D_MAX], dfw_p2: [[f64; crate::D_MAX+1]; crate::D_MAX+1], cost_params: (f64, f64, f64)) -> f64 {
+//     // Create a list of acceptable order quantites
+//     let mut acceptable_q: Vec<usize> = Vec::with_capacity(max_q + 1);
+//     acceptable_q.extend(0..=max_q);
     
 
+//     let (cu,co,cdfw) = cost_params;
+
+//     let mut best_q = (0, 0.0);
+
+//     for q in acceptable_q.iter() {
+//         // Calculate second stage expectation balancing shortage and holding costs
+//         let mut exp: f64 = 0.0;
+//         for (d1_val,d1_pmf_i) in demand_p1.iter().enumerate() {
+//             // Second stage expectation
+
+//             // On hand moving into the second stage
+//             let x_2 = f64::max(x as f64 - d1_val as f64, 0.0)+*q as f64;
+
+
+//             for (d2_val, d2_pmf_i) in demand_p2.iter().enumerate() {
+//                 let shortage_p2: usize = std::cmp::max(d2_val as isize - x_2 as isize,0) as usize;
+//                 for j in 0..shortage_p2+1 {
+//                     exp += d1_pmf_i * d2_pmf_i * dfw_p2[shortage_p2][j] * (cdfw * j as f64 + cu * (shortage_p2 - j) as f64);
+//                 }
+//                 exp += d1_pmf_i * d2_pmf_i * co * f64::max(x_2-d2_val as f64,0.0);
+
+//             }
+//         }
+//         // Check if the expectation is better than the current best
+//         if q == &0 {
+//             best_q.0 = *q;
+//             best_q.1 = exp;
+//         } else {
+//             if exp <= best_q.1 {
+//                 best_q.0 = *q;
+//                 best_q.1 = exp;
+//             }
+//         }
+//     }
+
+//     return best_q.0 as f64;
+// } 
+
+// Minimise q using a naive search
+pub fn minimise_q_search(max_q: f64, x: usize,demand_p1: [f64; crate::D_MAX], demand_p2: [f64; crate::D_MAX], dfw_p2: [[f64; crate::D_MAX+1]; crate::D_MAX+1], cost_params: (f64, f64, f64)) -> f64 {
+    // Pick mid-point
+    let mut q_mid = f64::floor(max_q / 2.0);
+
+    if q_mid == 0.0 {
+        return 0.0;
+    }
+    
+    let q_mid_res = expectation(x, q_mid, demand_p1, demand_p2, dfw_p2, cost_params);
+    let q_plus_1 = expectation(x, q_mid + 1.0, demand_p1, demand_p2, dfw_p2, cost_params);
+    let q_minus_1 = expectation(x, q_mid - 1.0, demand_p1, demand_p2, dfw_p2, cost_params);
+
+    let best = f64::min(q_mid_res, f64::min(q_plus_1, q_minus_1));
+    if best == q_mid_res {
+        return q_mid;
+    } 
+
+    // Get direction to move in
+    let dir: f64 = (if best == q_plus_1 {1.0} else {-1.0} as f64);
+
+    loop {
+        let q_mid_res = expectation(x, q_mid, demand_p1, demand_p2, dfw_p2, cost_params);
+        q_mid += dir;
+
+        let q_mid_res_change = expectation(x, q_mid, demand_p1, demand_p2, dfw_p2, cost_params);
+        if q_mid_res_change > q_mid_res {
+            return q_mid-dir;
+        }
+
+        if q_mid == 0.0 || q_mid == max_q {
+            if f64::min(q_mid_res, q_mid_res_change) == q_mid_res {
+                return q_mid - dir;
+            } else {
+                return q_mid;
+            }
+        }
+        
+
+    }
+    
+
+} 
+
+fn expectation(x: usize, q: f64, demand_p1: [f64; crate::D_MAX], demand_p2: [f64; crate::D_MAX], dfw_p2: [[f64; crate::D_MAX+1]; crate::D_MAX+1], cost_params: (f64, f64, f64)) -> f64 {
+    // Parameters
     let (cu,co,cdfw) = cost_params;
 
-    let mut best_q = (0, 0.0);
+    // Calculate second stage expectation balancing shortage and holding costs
+    let mut exp: f64 = 0.0;
+    for (d1_val,d1_pmf_i) in demand_p1.iter().enumerate() {
+        // Second stage expectation
 
-    for q in acceptable_q.iter() {
-        // Calculate second stage expectation balancing shortage and holding costs
-        let mut exp: f64 = 0.0;
-        for (d1_val,d1_pmf_i) in demand_p1.iter().enumerate() {
-            // Second stage expectation
-
-            // On hand moving into the second stage
-            let x_2 = f64::max(x as f64 - d1_val as f64, 0.0)+*q as f64;
+        // On hand moving into the second stage
+        let x_2 = f64::max(x as f64 - d1_val as f64, 0.0)+q as f64;
 
 
-            for (d2_val, d2_pmf_i) in demand_p2.iter().enumerate() {
-                let shortage_p2: usize = std::cmp::max(d2_val as isize - x_2 as isize,0) as usize;
-                for j in 0..shortage_p2+1 {
-                    exp += d1_pmf_i * d2_pmf_i * dfw_p2[shortage_p2][j] * (cdfw * j as f64 + cu * (shortage_p2 - j) as f64);
-                }
-                exp += d1_pmf_i * d2_pmf_i * co * f64::max(x_2-d2_val as f64,0.0);
-
+        for (d2_val, d2_pmf_i) in demand_p2.iter().enumerate() {
+            let shortage_p2: usize = std::cmp::max(d2_val as isize - x_2 as isize,0) as usize;
+            for j in 0..shortage_p2+1 {
+                exp += d1_pmf_i * d2_pmf_i * dfw_p2[shortage_p2][j] * (cdfw * j as f64 + cu * (shortage_p2 - j) as f64);
             }
-        }
-        // Check if the expectation is better than the current best
-        if q == &0 {
-            best_q.0 = *q;
-            best_q.1 = exp;
-        } else {
-            if exp < best_q.1 {
-                best_q.0 = *q;
-                best_q.1 = exp;
-            }
+            exp += d1_pmf_i * d2_pmf_i * co * f64::max(x_2-d2_val as f64,0.0);
+
         }
     }
+    return exp;
 
-    return best_q.0 as f64;
 }
+
